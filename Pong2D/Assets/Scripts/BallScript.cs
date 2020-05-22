bc2e06688715080b7a414944656f646f7120a80e
Justinayeung <yeunj511@newschool.edu>
2020-05-22 16:17:23 -0400

    Game Manager, Ball Bounce, and audio

--- Pong2D/Assets/Scripts/BallScript.cs
+++ Pong2D/Assets/Scripts/BallScript.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class BallScript : MonoBehaviour
 {
+    public AudioSource bounce;
+
+    private float speed;
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start(){
+        rb = GetComponent<Rigidbody2D>(); //Reference to rigidbody2D
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        speed = 8f; //Set speed to 8
+    }
+
+    /// <summary>
+    /// Get a random direction
+    /// </summary>
+    public void RandomDirection() {
+        //condition ? True : False
+        float x = Random.Range(0f, 2f) <= 1 ? -1 : 1;
+        float y = Random.Range(0f, 2f) <= 1 ? -1 : 1;
+        rb.velocity = new Vector2(speed * x, speed * y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("Border")) { //If ball hits border or player, play audio
+            bounce.pitch = Random.Range(0.65f, 1.15f); //Random audio pitch
+            bounce.Play(); //Play audio
+        }
     }
 }
--- Pong2D/Assets/Scripts/GameManager.cs
+++ Pong2D/Assets/Scripts/GameManager.cs
@@ -1,18 +1,87 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("Ball")]
+    public GameObject ball;
+
+    [Header("Player L")]
+    public Text L_Score;
+    public GameObject GoalL;
+    public GameObject PaddleL;
+
+    [Header("Player R")]
+    public Text R_Score;
+    public GameObject GoalR;
+    public GameObject PaddleR;
+
+    [Header("Text")]
+    public Text mainText;
+    public Text subText;
+
+    [Header("AudioSource")]
+    public AudioSource beep;
+
+    private int LScore;
+    private int RScore;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        LScore = 0;
+        RScore = 0;
+        mainText.color = Color.white;
+        subText.color = Color.white;
+        subText.text = "[SPACE] TO START";
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            mainText.color = Color.white; //Change color
+            mainText.text = " "; //Main text is blank
+            subText.text = " "; //Subtext is blank
+            subText.color = Color.white; //Change color
+            StartCoroutine(CountDown());
+        }
     }
+
+    /// <summary>
+    /// Game countdown
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator CountDown() {
+        mainText.text = "3"; //Change text
+        beep.Play(); //play beep audio
+        yield return new WaitForSeconds(1f); //Wait for one second
+        mainText.text = "2";
+        beep.Play();
+        yield return new WaitForSeconds(1f);
+        mainText.text = "1";
+        beep.Play();
+        yield return new WaitForSeconds(1f);
+        mainText.text = " ";
+        ball.GetComponent<BallScript>().RandomDirection(); //Send ball in random direction
+    }
+
+    /// <summary>
+    /// Add to Paddle L's score
+    /// </summary>
+    public void ScorePL() {
+        LScore++; //Add to score 
+        L_Score.text = RScore.ToString(); //Change text
+        StartCoroutine(CountDown()); //Restart countdown
+    }
+
+    /// <summary>
+    /// Add to Paddle R's score
+    /// </summary>
+    public void ScorePR() {
+        RScore++; //Add to score
+        R_Score.text = RScore.ToString(); //Change text
+        StartCoroutine(CountDown()); //Restart countdown
+    }
+
 }

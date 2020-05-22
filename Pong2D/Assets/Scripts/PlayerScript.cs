29c599e197970c4962513f79994d47f06d9ad055
Justinayeung <yeunj511@newschool.edu>
2020-05-22 16:00:02 -0400

    Player Movement and Post Processing

--- Pong2D/Assets/Scripts/PlayerScript.cs
+++ Pong2D/Assets/Scripts/PlayerScript.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    public bool PaddleL;
+    public float speed;
+
+    private Rigidbody2D rb;
+    private float movement;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        rb = GetComponent<Rigidbody2D>(); //Reference to rigidbody2D
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        PlayerMove();
+    }
+
+    public void PlayerMove()
+    {
+        if (PaddleL) {
+            movement = Input.GetAxisRaw("Vertical2"); //Left Paddle will move with WS
+        } else {
+            movement = Input.GetAxisRaw("Vertical1"); //Right Paddle will move with Arrow keys
+        }
+        rb.velocity = new Vector2(rb.velocity.x, movement * speed);
     }
 }

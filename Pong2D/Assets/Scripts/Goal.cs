65ac7e406bcf7400e82535692fc33cff940f34e2
Justinayeung <yeunj511@newschool.edu>
2020-05-22 16:22:13 -0400

    Goal Script

---
+++ Pong2D/Assets/Scripts/Goal.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    public bool GoalL;
+
+    /// <summary>
+    /// If the ball hits a border, add to score
+    /// </summary>
+    /// <param name="other"></param>
+    public void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Ball")) {
+            if (GoalL) {
+                GameObject.FindObjectOfType<GameManager>().ScorePR(); //Add to ScoreR if you hit GoalL
+            } else {
+                GameObject.FindObjectOfType<GameManager>().ScorePL(); //Add to ScoreL if you hit GoalR
+            }
+        }
+    }
+}
